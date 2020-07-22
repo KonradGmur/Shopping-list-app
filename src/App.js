273458e46f6c3cd25ff5d6a285ad48454c94aece
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-22 11:47:58 +0200

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ListItems from './ListItems'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+class App extends Component {
+  state = {}
+  render() {
+    return (  );
+  }
+}
+
+export default App;
