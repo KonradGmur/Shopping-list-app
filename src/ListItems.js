67d111ad3c2d9a17d77d1055ac189e5b265896ae
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-22 11:49:12 +0200

    Update ListItems.sj

---
+++ src/ListItems.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './ListItems.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import FlipMove from 'react-flip-move';
+
+const ListItems = () => {
+    return (  );
+}
+
+export default ListItems;
